256f4dc3bb9b46696d3e7c60d508719622a39dbc
jaennil <jaennil@yandex.ru>
2026-04-24 17:55:03 +0300

    fix: export sentry frontend runtime defaults

--- frontend/docker-entrypoint.d/40-env-config.sh
+++ frontend/docker-entrypoint.d/40-env-config.sh
@@ -6,6 +6,11 @@ set -eu
 : "${SENTRY_RELEASE:=}"
 : "${SENTRY_FRONTEND_TRACES_SAMPLE_RATE:=0}"
 
+export SENTRY_FRONTEND_DSN
+export SENTRY_ENVIRONMENT
+export SENTRY_RELEASE
+export SENTRY_FRONTEND_TRACES_SAMPLE_RATE
+
 envsubst '${SENTRY_FRONTEND_DSN} ${SENTRY_ENVIRONMENT} ${SENTRY_RELEASE} ${SENTRY_FRONTEND_TRACES_SAMPLE_RATE}' \
   < /usr/share/nginx/html/env.template.js \
   > /usr/share/nginx/html/env.js
